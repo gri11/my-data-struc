c50bba894ea881f08d85175c80cf702bec795977
grill <kanawatgrill2002@gmail.com>
2022-10-23 13:51:09 +0700

    Pass vector_no_move

--- no_move/student.h
+++ no_move/student.h
@@ -5,13 +5,29 @@
 
 template <typename T>
 T& CP::vector_no_move<T>::operator[](int idx) {
-  //your code here
+  // your code here
+  auto lb = std::lower_bound(aux.begin(), aux.end(), idx);
+  auto st = (*lb == idx) ? lb : lb-1; //start idx
+  int block = st - aux.begin(); 
+  int nidx = idx - *st;
+
+  // debug();
+  return mData[block][nidx];
 }
 
 template <typename T>
 void CP::vector_no_move<T>::expand_hook() {
-  //your code here
-  //you MAY need this function
+  // your code here
+  // you MAY need this function
+
+  // use aux to collect index that each vector starts? lower bound to find.
+  // aux = {};
+  if(mSize == 0) 
+    aux.push_back(0);
+  else
+    //             prev idx       
+    aux.push_back(aux.back() + (mData.end()-2)->size());
+
 }
 
 #endif
